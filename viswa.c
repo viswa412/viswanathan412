8f8f44962304475bb703d45510cca0afd49dafc4
viswa412 <43338039+viswa412@users.noreply.github.com>
2018-09-18 14:47:50 +0530

    Create viswa.c

---
+++ viswa.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+ #include<conio.h>
+void main()
+{
+    int i;
+     printf("Enter a number \n");
+    scanf("%d", &i);
+    if (i> 0)
+        printf("positive);
+    else if(i==0)
+        printf("zero");
+        else
+        printf("negative");
+        getch();
+    }
